a3433944e0ff1b070585644dc2e036f7ac79acb4
Sean <seanrforeman@yahoo.com>
2020-04-16 11:32:23 -0500

    completed vertex buffers episode

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -33,20 +33,34 @@ int main() {
 	// 4.6.0 = OpenGL version
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// ---------------------------------------------------------------------------
+	// Create a modern OpenGL buffer
+	// ---------------------------------------------------------------------------
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	// this creates a buffer and gives us back the id of the buffer
+	unsigned int bufferID;
+	glGenBuffers(1, &bufferID);
+
+	// Select the buffer by ID
+	glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+
+	// Fill the buffer with data
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
 	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window)) {
 		
 		// Render here
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// --------------------------------------------------------------------------
-		// Old OpenGL code to create a triangle
-		// --------------------------------------------------------------------------
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
+		// Use buffer
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// Swap front and back buffers
 		glfwSwapBuffers(window);
